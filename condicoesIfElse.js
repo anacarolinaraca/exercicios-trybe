0afad08bd93429958b3be6f966b2ab62df33462d
Ana Carolina Rodrigues <carolina_ralves@hotmail.com>
2023-04-04 19:29:25 -0300

    Condições if e else

---
+++ condicoesIfElse.js
@@ -0,0 +1,9 @@
+let notaCandidata = 80;
+
+if (notaCandidata >= 80){
+    console.log("Parabéns, você faz parte do grupo de pessoas aprovadas!"); 
+} else if (notaCandidata < 80 && notaCandidata >= 60) {
+    console.log("Você está na nossa lista de espera.");
+} else if (notaCandidata < 60) {
+    console.log("Infelizmente, você reprovou.");
+}
