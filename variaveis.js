64827085b20bb97deb93496a3060a6c1608d99bc
Ana Carolina Rodrigues <carolina_ralves@hotmail.com>
2023-04-04 16:18:52 -0300

    Variáveis

---
+++ variaveis.js
@@ -0,0 +1,5 @@
+let myName = "Ana Carolina";
+const birthCity = "Belo Horizonte";
+const birthYear = 1999;
+
+console.log("Meu nome é " + myName + ", sou de " + birthCity + " e nasci no ano de " + birthYear + ".");
