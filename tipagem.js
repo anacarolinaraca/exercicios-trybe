ddfd62175ca7ed2e6baf419bf95bf46c338b580e
Ana Carolina Rodrigues <carolina_ralves@hotmail.com>
2023-04-04 19:16:36 -0300

    Tipos Primitivos, Tipagem Dinâmica e Operações Aritméticas

---
+++ tipagem.js
@@ -0,0 +1,7 @@
+let base = 5;
+let height = 8;
+let area = base * height;
+
+let perimeter = (2 * base) + (2 * height);
+
+console.log("A área do retângulo é " + area + " e o perímetro é " + perimeter + ".");
