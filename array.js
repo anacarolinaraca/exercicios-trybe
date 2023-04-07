f82d93fa609487203650fb34a58d7fdf7b3219ae
Ana Carolina Rodrigues <carolina_ralves@hotmail.com>
2023-04-07 16:28:23 -0300

    Arrays (listas)

---
+++ array.js
@@ -0,0 +1,8 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu.length;
+let indexOfPortfolio = menu.indexOf();
+menu.push('Contato');
+
+console.log(menuServices);
+console.log(indexOfPortfolio);
+console.log(menu);
