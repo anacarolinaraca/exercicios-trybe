ce488d6df8f5a4de71e9d473147c55a81587b312
Ana Carolina Rodrigues <carolina_ralves@hotmail.com>
2023-04-11 10:55:04 -0300

    for

---
+++ for.js
@@ -0,0 +1,6 @@
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let lista = 0; lista < groceryList.length; lista += 1) {
+   let element = groceryList[lista];
+   console.log(element);
+}
