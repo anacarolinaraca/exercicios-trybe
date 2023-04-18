98c983c022856b4d3a02f71c82de0681e08940ff
Ana Carolina Rodrigues <carolina_ralves@hotmail.com>
2023-04-18 20:21:04 -0300

    Codigo 4

--- exercicio1.js
+++ exercicio1.js
@@ -24,6 +24,20 @@
 
 // MUDAR CONST PARA LET
 
-let favoriteFood = 'Lasanha';
-favoriteFood = 'Hambúrguer';
-console.log(favoriteFood);
+// let favoriteFood = 'Lasanha';
+// favoriteFood = 'Hambúrguer';
+// console.log(favoriteFood);
+
+
+// TEMPLATE LITERALS
+
+const name = 'Adriana';
+const lastName = 'Soares';
+console.log(`Olá, ${name} ${lastName}!`);
+function soma(a,b) {
+  let resultado = a + b;
+  return resultado;
+}
+let a = 3;
+let b = 5;
+console.log(`O resultado da soma de ${a} + ${b} é: ${soma(a,b)}`);
