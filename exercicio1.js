55f9cb84b1daa75c80b4bc007c5a2f872dc0ff44
Ana Carolina Rodrigues <carolina_ralves@hotmail.com>
2023-04-18 20:16:23 -0300

    Codigo 2

--- exercicio1.js
+++ exercicio1.js
@@ -1,6 +1,22 @@
-function imprimeIdade() {
-    for (let idade = 30; idade <= 40; idade += 1) {
-      console.log('Idade dentro do for:', idade)
-    }
-}
-imprimeIdade()
+// MUDANÇA VAR PARA LET E APAGAR CONSOLE FORA DO ESCOPO
+
+// function imprimeIdade() {
+//     for (let idade = 30; idade <= 40; idade += 1) {
+//       console.log('Idade dentro do for:', idade)
+//     }
+// }
+// imprimeIdade()
+
+
+// MUDAR CONST PARA LET
+
+  let pessoa = {
+    nome: 'Henri',
+    idade: 20
+  }
+  pessoa = {
+    nome: 'Luna',
+    idade: 19
+  } 
+  console.log('Nome:', pessoa.nome);
+  console.log('Idade:', pessoa.idade);
